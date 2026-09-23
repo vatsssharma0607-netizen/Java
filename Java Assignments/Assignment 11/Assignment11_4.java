c2b07baa9599755dc435cb9a186ce6650ebbfeb0
Vattss Sharma <vatsssharma0607@gmail.com>
2026-09-23 10:31:37 +0530

    Update Assignment11_4.java

--- Java Assignments/Assignment 11/Assignment11_4.java
+++ Java Assignments/Assignment 11/Assignment11_4.java
@@ -1,1 +1,18 @@
-Calculator 
+public class Calculator {
+    interface MathOperation {
+        double operate(double a, double b);
+    }
+
+    public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation subtract = (a, b) -> a - b;
+        MathOperation multiply = (a, b) -> a * b;
+        MathOperation divide = (a, b) -> a / b;
+        double x = 10.0;
+        double y = 2.0;
+        System.out.println("Addition: " + add.operate(x, y));
+        System.out.println("Subtraction: " + subtract.operate(x, y));
+        System.out.println("Multiplication: " + multiply.operate(x, y));
+        System.out.println("Division: " + divide.operate(x, y));
+    }
+}
